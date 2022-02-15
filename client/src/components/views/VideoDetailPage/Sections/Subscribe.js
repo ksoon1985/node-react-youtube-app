c6f44ae84def2e232346e2ea14691ee230ce1c59
ksoon1985 <ksoon1985@naver.com>
2022-02-16 02:32:20 +0900

    구독 추가 삭제 기능 추가

---
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -0,0 +1,88 @@
+import Axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+
+function Subscribe(props) {
+
+    const [SubscribeNumber, setSubscribeNumber] = useState(0);
+    const [Subscribed, setSubscribed] = useState(false);    
+
+    useEffect(() => {
+        
+        let variable = {userTo: props.userTo}
+
+        // 게시글을 올린 유저의 구독자 수 가져오기 
+        Axios.post('/api/subscribe/subscribeNumber',variable)
+        .then(response=>{
+            if(response.data.success){
+                setSubscribeNumber(response.data.subscribeNumber)
+            }else{
+                alert('구독자 수 정보를 받아오지 못했습니다. ')
+            }
+        })
+
+        // userTo : 게시글 올린 User, userFrom : 로그인한 User
+        let subscribedVariable = {userTo: props.userTo, userFrom: localStorage.getItem('userId')}
+
+        // 구독 했는지 안했는지 
+        Axios.post('/api/subscribe/subscribed',subscribedVariable)
+        .then(response=>{
+            if(response.data.success){
+                setSubscribed(response.data.subscribed)
+            }else{
+                alert('정보를 받아오지 못했습니다.')
+            }
+        })
+
+    },[])
+
+    const onSubscribe = () =>{
+
+        let subscribedVariable = {
+            userTo: props.userTo,
+            userFrom: props.userFrom
+        }
+
+        //이미 구독 중이라면
+        if(Subscribed){
+            Axios.post('/api/subscribe/unSubscribe',subscribedVariable)
+            .then(response=>{
+                if(response.data.success){
+                    setSubscribeNumber(SubscribeNumber - 1)
+                    setSubscribed(!Subscribed)
+                }else{
+                    alert('구독 취소 하는데 실패 했습니다.')
+                }
+            })
+
+        //구독하지 않았다면
+        }else{
+            Axios.post('/api/subscribe/subscribe',subscribedVariable)
+            .then(response=>{
+                if(response.data.success){
+                    setSubscribeNumber(SubscribeNumber + 1)
+                    setSubscribed(!Subscribed)
+                }else{
+                    alert('구독 하는데 실패 했습니다.')
+                }
+            })
+        }
+    }
+
+  return (
+    <div>
+        <button  
+            style={{
+                backgroundColor: `${Subscribed ? '#AAAAAA' : '#CC0000'}` , borderRadius: '4px',
+                color:'white', padding: '10px 16px',
+                fontWeight:'500', fontSize:'1rem', textTransform:'uppercase'
+            }}
+            onClick={onSubscribe}
+        >
+            {SubscribeNumber} {Subscribed ? 'Subscribed' : 'Subscribe'}
+        </button>
+    </div>
+  )
+}
+
+export default Subscribe
